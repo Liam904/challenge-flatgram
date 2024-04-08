c13cce815d9d4c5e5f7c5570a86a90555b1fd8dc
Liam <lw433891@gmail.com>
2024-04-08 18:24:04 +0300

    'fixes'

--- src/index.js
+++ src/index.js
@@ -23,19 +23,44 @@ const form = document.getElementById("comment-form");
 
 
 function addComment() {
+  const requestOptions = {
+    method: "POST",
+    redirect: "follow"
+  };
+
   
-
-
-
-  form.addEventListener('submit', function(event) {
-    
-    event.preventDefault()
-
-    const commentText = commentInput.value;
-    const newComment = document.createElement('li');
-    newComment.innerText = commentText;
-    commentsList.appendChild(newComment);
+function addComment(content) {
+  fetch('http://localhost:3000/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ content }),
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log('New comment added:', result);
+    updateDOM(result.content);
+  })
+  .catch(error => {
+    console.error('Error adding comment:', error);
   });
+}
+
+
+function updateDOM(content) {
+  const newComment = document.createElement('li');
+  newComment.innerText = content;
+  commentsList.appendChild(newComment);
+}
+
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const commentText = commentInput.value;
+  addComment(commentText);
+});
+
 }
 
 function addCount(){
